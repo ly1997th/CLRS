9488521e8f6d430a8111e97eb29f9f9d692b215b
ly1997th <ly_dra@163.com>
2019-03-24 10:08:10 +0800

    change 2.1.2

--- source/2_1_2.cpp
+++ source/2_1_2.cpp
@@ -2,13 +2,33 @@
 
 using namespace std;
 
+template<class T>
+void insertion_sort(T A[10]);
+
 int main()
 {
-
-    for(int i=1;i<10;++i)
-    {
-        cout<<i<<endl;
-    }
+    float A[10];
+    for(int i=0;i<10;++i)
+        A[i]=rand()/10.0;
+    insertion_sort(A);
+    for(int i=0;i<10;++i)
+        cout<<A[i]<<" ";
     return 0;
 }
 
+//排序，使用降序排列
+template<class T>
+void insertion_sort(T A[10])
+{
+    for(int j=1;j<10;++j)
+    {
+        T key=A[j];
+        int i=j-1;
+        while(i>=0&&A[i]<key)
+        {
+            A[i+1]=A[i];
+            i=i-1;
+        }
+        A[i+1]=key;
+    }
+}
