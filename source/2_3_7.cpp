dbe1aff25a82f4ee3ab69df0fb4fcbb824d3ba9d
ly1997th <ly_dra@163.com>
2019-03-31 17:33:38 +0800

    twoSum with the violent search

--- source/2_3_7.cpp
+++ source/2_3_7.cpp
@@ -9,46 +9,51 @@ template <class T>
 void merge(T A[],int p,int q,int r);
 
 template <class T>
-void merge_use(T A[],int length);
-
-template<class T>
-void addToX(T A[],T B[],int length,T v);
+int TwoSum(T A[],T V,T re[],int length);
 
 int main()
 {
-    float A[10]={10},B[10];
+    //算法测试
+    int A[4]={1,3,2,4};
     int length=sizeof(A)/sizeof(A[0]);
-    // for(int i=0;i<length;++i)
-    //     A[i]=rand()/10.0;
-    merge_use(A,length);
-    float v=10;
-    addToX(A,B,length,v);
+    for(int i=0;i<length;++i)
+        cout<<A[i]<<" ";
+    cout<<endl;
+    merge_sort(A,0,length-1);
+    int V=5;int re[length*2];
+    int num=TwoSum(A,V,re,length);
+    cout<<V<<":";
+    for(int i=1;i<=num;++i)
+    {
+        cout<<"<"<<re[(i-1)*2]<<","<<re[i*2-1]<<">"<<" ";
+    }
     return 0;
 }
 
-//查找元素
-template<class T>
-void addToX(T A[],T B[],int length,T v)
-{
-    int end=length-1;
-    int start=0;
-    while((start+end)/2>=0)
-    {
-        if(A[(start+end)/2]==v) return (start+end)/2;
-        if(A[(start+end)/2]<v)  start=(start+end)/2+1;
-        if(A[(start+end)/2]>v)  end=(start+end)/2-1;
-        if(start==end&&A[start]!=v) return -1;
-    }
-}
 
-//封装
 template <class T>
-void merge_use(T A[],int length)
+int TwoSum(T A[],T V,T re[],int length)
 {
-    merge_sort(A,0,length-1);
+    if(A[0]>V) return 0;
+    int p=0;
+    for(int i=0;i<length-1;++i)
+    {
+        if(A[i]>V) break;
+        else
+        {
+            int j=i+1;
+            while(A[j]<=V&&j<length)
+            {
+                if(A[i]+A[j]==V) 
+                {re[p]=A[i];re[++p]=A[j];++p;}
+                ++j;
+            }
+        }        
+    }
+    return p/2;
 }
 
-//归并排序,递归排序,按非降序
+//归并排序,递归排序
 template <class T>
 void merge_sort(T A[],int p,int r)
 {
@@ -65,17 +70,19 @@ void merge_sort(T A[],int p,int r)
 template <class T>
 void merge(T A[],int p,int q,int r)
 {
-    T M[q-p+1],N[r-q];    
+    T M[q-p+2],N[r-q+1];
     for(int i=0;i<q-p+1;++i)
         M[i]=A[p+i];
     for(int i=0;i<r-q;++i)
         N[i]=A[q+i+1];
+    M[q-p+1]=INT_MAX;N[r-q]=INT_MAX;  //哨兵，检查是否已经完成某个子序列的排序
     int i=0,j=0;
     for(int k=p;k<=r;++k)
     {
-        if((M[i]<=N[j]&&i<q-p+1)||j==r-q)   //检查某个序列是否已经完成合并排序
+        if(M[i]<=N[j])
             {A[k]=M[i];++i;}
         else
             {A[k]=N[j];++j;}
     }
+
 }
